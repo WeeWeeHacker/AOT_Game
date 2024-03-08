c39c24f48dad5b267a1c42edd70c87700b18c19a
Moaz <155671880+WeeWeeHacker@users.noreply.github.com>
2024-03-08 16:18:14 +0200

    Update AbnormalTitan.java

--- src/game/engine/titans/AbnormalTitan.java
+++ src/game/engine/titans/AbnormalTitan.java
@@ -1,2 +1,24 @@
-package game.engine.titans;public class AbnormalTItan {
+package game.engine.titans;
+public class AbnormalTitan extends Titan {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void getAttacked() {
+
+    }
+
+    public AbnormalTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase,
+                         int speed, int resourcesValue, int dangerLevel){
+        super(baseHealth, baseDamage, heightInMeters, distanceFromBase, speed, resourcesValue, dangerLevel);
+    }
+
 }
